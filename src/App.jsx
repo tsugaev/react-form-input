eb4a7f02835e4915865d973cc19e27424841f9eb
Shamil <shamil>
2022-05-20 02:52:25 +0300

    forms done

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+import React from "react";
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [text, setText] = useState("");
+  const [sended, setSended] = useState(false);
+  const [blur, setBlur] = useState(false);
+
+  const handleChange = (e) => {
+    setBlur(false);
+    setSended(false);
+    setText(e.target.value);
+  };
+
+  const blurHandler = (e) => {
+    if (text === "") {
+      setSended(false);
+      setBlur(true);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    if (text !== "") {
+      e.preventDefault();
+      setText("");
+      setSended(true);
+      console.log(text);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          onBlur={blurHandler}
+          type="text"
+          className={blur ? "is-error" : "input-text"}
+          value={text}
+          onChange={handleChange}
+        />
+        <button className="btn" disabled={blur ? "disabled" : ""} type="submit">
+          Отправить
+        </button>
+      </form>
+      {sended && (
+        <div className="send-message">Сообщение успешно отправлено</div>
+      )}
+      {blur && (
+        <div style={{ color: "red" }}>Поле ввода не должно быть пустым</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
